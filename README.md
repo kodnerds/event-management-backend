cfc32b981cfe3f4e5fd3733292ccb3d9f3c3835a
John Oshalusi <john.oshalusi@iohk.io>
2025-10-01 21:09:57 +0100

    feat: formating improvements

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Awesome Project Build with TypeORM
+# Event Management Backend
 
 Steps to run this project:
+- Clone this repo
+- Run `npm install` to install dependencies
+- Create a `.env` file in the root directory and copy the `.env.example` and set your own values
+- Run app with `npm run dev`
 
-1. Run `npm i` command
-2. Setup database settings inside `data-source.ts` file
-3. Run `npm start` command
+## Commit convention
+- Use [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) for commit messages
+- This project uses husky to enforce commit message format.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3001;
@@ -10,12 +10,12 @@ export const main = async () => {
   app.use(express.urlencoded({ extended: true }));
 
   app.get('/', (_: Request, res: Response) => {
-    return res.send({message: 'Welcome to Event management API'})
-  })
+    return res.send({ message: 'Welcome to Event management API' });
+  });
 
   app.listen(PORT, () => {
     console.info(`Server listening on port http://localhost:${PORT}`);
   });
-}
+};
 
 main();
