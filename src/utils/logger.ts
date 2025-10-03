e7198c5dd2eea2cbae8d6912008e1b329b8b0ed6
fmdavids <fmproade@gmail.com>
2025-10-03 23:45:31 +0100

    feat: implement logger as json file and control logger level from .env

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,25 @@
+import path from "path";
 import { createLogger, format, transports } from "winston";
 
+const logLevel = process.env.LOG_LEVEL || "info"
 const logger = createLogger({
   level: "info",
   format: format.combine(
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     format.colorize(),
-    format.printf(({ level, message, timestamp }) => {
-      return `${timestamp} [${level}] ${message}`;
-    })
+    format.json()
   ),
   transports: [
-    new transports.Console(),
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
+    new transports.Console(), 
+    new transports.File({
+        filename: path.join(__dirname, "logs/app.log"), // log file path
+        level: "info",
+      }),
+      new transports.File({
+        filename: path.join(__dirname, "logs/error.log"),
+        level: "error",
+      }),
+    
   ],
 });
 
