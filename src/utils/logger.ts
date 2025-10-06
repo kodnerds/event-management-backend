763d9a6ad7831e96adbae69d0b9f88b9ca732a8d
fmdavids <fmproade@gmail.com>
2025-10-06 18:50:24 +0100

    feat: removed log file and formated

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,26 +1,25 @@
-import path from "path";
-import { createLogger, format, transports } from "winston";
+import path from 'path';
+import { createLogger, format, transports } from 'winston';
 
-const logLevel = process.env.LOG_LEVEL || "info"
+const logLevel = process.env.LOG_LEVEL || 'info';
 const logger = createLogger({
-  level: "info",
+  level: 'info',
   format: format.combine(
-    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.colorize(),
     format.json()
   ),
   transports: [
-    new transports.Console(), 
+    new transports.Console(),
     new transports.File({
-        filename: path.join(__dirname, "logs/app.log"), // log file path
-        level: "info",
-      }),
-      new transports.File({
-        filename: path.join(__dirname, "logs/error.log"),
-        level: "error",
-      }),
-    
-  ],
+      filename: path.join(__dirname, '../logs/app.log'),
+      level: 'info'
+    }),
+    new transports.File({
+      filename: path.join(__dirname, '../logs/error.log'),
+      level: 'error'
+    })
+  ]
 });
 
 export default logger;
