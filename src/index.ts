c56e249a3dd9288e1fed7e16c7fc90ea105b4bcf
John Oshalusi <john.oshalusi@iohk.io>
2025-10-06 23:13:24 +0100

    feat: setup eslint (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ?? 3001;
 
-export const main = async () => {
+export const main = () => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.get('/', (_: Request, res: Response) => {
-    return res.send({ message: 'Welcome to Event management API' });
-  });
+  app.get('/', (_: Request, res: Response) =>
+    res.send({ message: 'Welcome to Event management API' })
+  );
 
   app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.info(`Server listening on port http://localhost:${PORT}`);
   });
 };
