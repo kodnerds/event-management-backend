ccd9f8eab8a0ae3e967b700aa7316632d55bc5f6
fmdavids <fmproade@gmail.com>
2025-10-08 22:21:45 +0100

    feat: update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,7 +12,7 @@ const logger = createLogger({
   transports: [
     new transports.Console(),
     new transports.File({
-      filename: path.join(__dirname, '../logs/app.log'),
+      filename: path.join(__dirname, '../logs/app.log'), 
       level: 'info'
     }),
     new transports.File({
